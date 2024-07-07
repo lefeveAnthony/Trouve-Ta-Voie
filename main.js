86187a57a916b46f328b95960077e7f4e5c165cd
lefeveAnthony <lefeve.anthony@gmail.com>
2024-07-07 19:36:52 +0200

    Initialisation du projet

---
+++ main.js
@@ -0,0 +1,85 @@
+let voies = [
+  "1-vert",
+  "1-noir",
+  "1-blanc",
+  "2-",
+  "3-violet",
+  "3-vert",
+  "4-blanc",
+  "4-jaune",
+  "4-orange",
+  "5-rouge",
+  "5-vert",
+  "5-bleu",
+  "6-orange",
+  "6-noir/blanc",
+  "6-Rouge+vert",
+  "6-Rouge",
+  "7-jaune",
+  "7-bleu",
+  "7-vert",
+  "8-blanc",
+  "8-marron",
+  "8-rouge",
+  "9-orange",
+  "9-vert",
+  "9-rose",
+  "10-bleu",
+  "10-rouge",
+  "10-blanc",
+  "10-noir",
+  "11-vert",
+  "11-jaune",
+  "11-orange",
+  "12-jaune",
+  "12-violet",
+  "12-bleu",
+  "13-rose",
+  "13-violet",
+  "13-marbrée",
+  "13-blanc",
+  "14-jaune",
+  "14-blanc",
+  "14-rouge",
+  "15-vert",
+  "15-gris",
+  "15-violet",
+  "16-jaune",
+  "16-orange",
+  "16-blanc",
+  "16-gris",
+  "17-rouge",
+  "17-orange",
+  "17-bleu",
+  "17-violet",
+  "18-blanc",
+  "18-jaune",
+  "18-noir",
+  "19-bleu",
+  "19-gris",
+  "19-violet",
+  "20-jaune",
+  "20-rouge",
+  "20-vert",
+  "21-gris",
+  "21-bleu",
+  "21-orange",
+  "21-violet",
+  "22-noir",
+  "22-violet",
+  "22-rouge",
+  "22-vert"
+];
+
+
+function getNb(max){
+  return Math.floor(Math.random() * max);
+  
+}
+
+function tirer() {
+  let tab = voies.length + 1;
+  let result = voies[getNb(tab)].split("-");
+  let text = document.getElementById("result").innerText = "Numéro de mur : " + result[0]  + "\nCouleur de la voie : " + result[1];
+}
+
